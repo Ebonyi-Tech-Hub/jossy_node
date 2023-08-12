29f2ba9a5a66ee8821e24dfd04aea071fcfc0f92
Jossyng <135830966+Jossyng@users.noreply.github.com>
2023-08-12 16:05:58 +0100

    Update tutorialTwo.js

--- tutorialTwo.js
+++ tutorialTwo.js
@@ -2,12 +2,12 @@ let g = 1;
 let y = 2;
 let m = 3;
 let a = g+y;
-let b = a×a;
+let b = a*a;
 let d = a÷a;
-let f = d×b;
+let f = d*b;
 let h = 3+m;
 let e = h÷a;
 let k = e÷m;
-let n = 4×k;
+let n = 4*k;
 let c = f-n;
 console.log(c)
